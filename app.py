fbf3a5cff405b2c1d195d3f80e59b84bc0b6fcf9
Chiyun-Lee <chiyun.alberto.lee@gmail.com>
2026-03-30 14:53:00 +0100

    added home page

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ import streamlit as st
 st.set_page_config(layout="wide", page_title="Variant Marketplace", page_icon="assets/logo.svg")
 st.logo("assets/logo.svg", size="large")
 
-order_page   = st.Page("pages/order.py",   title="Order",   icon=":material/shopping_cart:")
-checkout_page = st.Page("pages/inspect.py", title="Checkout", icon=":material/dataset_linked:")
+home_page     = st.Page("pages/home.py",     title="Home",     icon=":material/home:")
+order_page    = st.Page("pages/order.py",    title="Order",    icon=":material/shopping_cart:")
+checkout_page = st.Page("pages/checkout.py", title="Checkout", icon=":material/dataset_linked:")
 
-pg = st.navigation([order_page, checkout_page])
+pg = st.navigation([home_page, order_page, checkout_page])
 pg.run()
